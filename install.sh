5c62c53b91c561f7321f82e0f7c3cc19b07c794d
Max Bruckner <max@maxbruckner.de>
2015-06-08 17:32:11 +0200

    Install-Script

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+if [[ "$(readlink -f "$0")" != "$(readlink -f ~/config/install.sh)" ]]; then
+   echo config needs to be installed in ~/config!
+   exit 1
+fi
+
+# Files that get symlinked to home
+HOME_FILES=( .ansi-colors .bashrc-common .tmux.conf .vimrc .vim .nvim .nvimrc .git_template )
+
+
+for file in ${HOME_FILES[@]}; do
+   if [[ ! -e ~/$file ]]; then
+      ln -sv ~/config/$file ~/$file
+   else
+      echo ~/$file already exists, omitting
+   fi
+done
+
+hash git 2> /dev/null && git config --global init.templatedir '~/.git_template'
+hash git 2> /dev/null && git config --global ui.color true 
+
+hash vim 2> /dev/null && vim -c ':PlugInstall'
+hash vim 2> /dev/null && vim -c ':GitGutterEnable'
+
+cd ~/config/.vim/plugged/YouCompleteMe
+./install.sh --clang-completer --omnisharp-completer
+
+echo Enable the bash config by adding the following to your ~/.bashrc:
+echo GITBRANCH=''
+echo source ~/.bashrc-config
+
