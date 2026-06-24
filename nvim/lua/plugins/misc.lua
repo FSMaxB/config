4c0412a1dc8f1cd1b9aed6f6c979ece98562fa34
Max Bruckner <max@c.amo.co>
2026-06-24 13:06:21 +0200

    nvim: Add fidget for rust-analyzer progress indicator

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -1,3 +1,4 @@
 return {
 	{ "RaafatTurki/hex.nvim", cmd = { "HexToggle", "HexDump", "HexAssemble" }, opts = {} },
+	{ "j-hui/fidget.nvim", event = "LspAttach", opts = {} }, -- LSP progress popup (rust_analyzer indexing, etc.)
 }
