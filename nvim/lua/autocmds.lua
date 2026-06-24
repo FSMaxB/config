17d050cd11413ca847972666bc0421618d8cb3d9
Max Bruckner <max@c.amo.co>
2026-06-24 15:32:58 +0200

    nvim: rust-analyzer inlay hints

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -4,3 +4,14 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.hl.on_yank()
 	end,
 })
+
+-- enable inlay hints (e.g. rust-analyzer inline type annotations) for any
+-- buffer whose attached LSP supports them
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if client and client:supports_method("textDocument/inlayHint") then
+			vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
+		end
+	end,
+})
