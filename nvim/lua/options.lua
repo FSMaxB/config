47f2e28e714b50009c8f14a58787d0c11559e461
Max Bruckner <max@c.amo.co>
2026-06-24 12:13:03 +0200

    neovim: Load local config from project at startup

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,17 +1,17 @@
-vim.opt.termguicolors = true          -- was nvim-only
-vim.opt.inccommand = "nosplit"        -- was nvim-only
+vim.opt.termguicolors = true -- was nvim-only
+vim.opt.inccommand = "nosplit" -- was nvim-only
 
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.ruler = true
-vim.opt.signcolumn = "yes"             -- stable gutter for git/diagnostic signs
+vim.opt.signcolumn = "yes" -- stable gutter for git/diagnostic signs
 vim.opt.laststatus = 2
 vim.opt.cmdheight = 2
 
 vim.opt.autoindent = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = false             -- noexpandtab
+vim.opt.expandtab = false -- noexpandtab
 vim.opt.backspace = { "indent", "eol", "start" }
 
 vim.opt.hlsearch = true
@@ -28,3 +28,6 @@ vim.opt.clipboard:append("unnamedplus")
 
 vim.opt.list = true
 vim.opt.listchars = { tab = "· ", extends = ">", precedes = "<", nbsp = "+" }
+
+-- Source a trusted, project-local `.nvim.lua`/`.nvimrc`/`.exrc` from the cwd at startup
+vim.opt.exrc = true
