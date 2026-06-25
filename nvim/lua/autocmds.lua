41fc9d7e29bce51933980a134cfe8dd5699d60aa
Max Bruckner <max@c.amo.co>
2026-06-25 11:26:52 +0200

    nvim: Use snacks notifier for LSP progress

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -15,3 +15,19 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		end
 	end,
 })
+
+-- route LSP progress ($/progress) through the Snacks notifier: a single toast
+-- that updates in place with an animated spinner, then a check mark on finish.
+vim.api.nvim_create_autocmd("LspProgress", {
+	callback = function(ev)
+		local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+		vim.notify(vim.lsp.status(), "info", {
+			id = "lsp_progress",
+			title = "LSP",
+			opts = function(notif)
+				notif.icon = ev.data.params.value.kind == "end" and " "
+					or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+			end,
+		})
+	end,
+})
--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -1,4 +1,3 @@
 return {
 	{ "RaafatTurki/hex.nvim", cmd = { "HexToggle", "HexDump", "HexAssemble" }, opts = {} },
-	{ "j-hui/fidget.nvim", event = "LspAttach", opts = {} }, -- LSP progress popup (rust_analyzer indexing, etc.)
 }
