bd526b815301c918ccff5685e88c9cb3489b1a1e
Max Bruckner <max@c.amo.co>
2026-08-28 09:27:26 +0200

    Fix jj.nvim picker keymap

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -11,7 +11,6 @@ return {
 			vim.keymap.set("n", "<leader>js", picker.status, { desc = "jj status (picker)" })
 			vim.keymap.set("n", "<leader>jh", picker.file_history, { desc = "jj file history (picker)" })
 			vim.keymap.set("n", "<leader>jc", picker.conflict, { desc = "jj conflicts (picker)" })
-			vim.keymap.set("n", "<leader>jx", picker.conflict_sections, { desc = "jj conflict sections (picker)" })
 		end,
 	},
 	{
